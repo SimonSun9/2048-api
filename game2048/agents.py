68fd7fceefff2d973fb551f06115207c92d0149c
SimonSun9 <40719680+SimonSun9@users.noreply.github.com>
2020-06-20 22:41:58 +0800

    Update agents.py

--- game2048/agents.py
+++ game2048/agents.py
@@ -24,7 +24,33 @@ class Agent:
     def step(self):
         direction = int(input("0: left, 1: down, 2: right, 3: up = ")) % 4
         return direction
+    
+class MyAgent(Agent):
 
+    def __init__(self, game, display=None):
+        super().__init__(game, display)
+        self.model_256 = net.RNN()
+        self.model_512 = net.RNN()
+        self.model_1024 = net.RNN()
+        self.model_256.load_state_dict(torch.load('model/model128.pkl', map_location=torch.device('cpu')))
+        self.model_512.load_state_dict(torch.load('model/model512.pkl', map_location=torch.device('cpu')))
+        self.model_1024.load_state_dict(torch.load('model/model1024.pkl', map_location=torch.device('cpu')))
+
+    def step(self):
+        board1 = self.game.board
+        board1[board1 == 0] = 1
+        board1 = np.log2(board1)
+        board2 = np.transpose(board1)
+        input = np.vstack((board1, board2)).reshape((1, 8, 4))
+        input = torch.from_numpy(input)
+        if np.max(board1) < 8:
+            output = self.model_256(input.float())
+        elif np.max(board1) < 9:
+            output = self.model_512(input.float())
+        else:
+            output = self.model_1024(input.float())
+        direction = torch.max(output, 1)[1]
+        return direction
 
 class RandomAgent(Agent):
 
